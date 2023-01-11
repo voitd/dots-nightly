54bcace7f65e273f630fef40199a01de5c417318
voitd <dimytr.voitenko@gmail.com>
2023-01-11 19:10:02 +0200

    chore: added some aliases

--- .config/fish/abbreviations/main.fish
+++ .config/fish/abbreviations/main.fish
@@ -1,10 +1,13 @@
 # General
-abbr -ag ls "exa -Slhg --icons"
-abbr -ag lsa "exa -Slhga --icons"
+abbr -ag ls "exa -G  --color auto --icons -a -s type"
+abbr -ag ll "exa -alhF --group-directories-first --time-style=long-iso"
+
 abbr -ag tree "exa -SlhgT --icons"
 abbr -ag r "ranger"
 abbr -ag n "nvim"
 abbr -ag cc "clear"
+
+abbr -ag python "python3"
 
 abbr gist 'pbpaste | gh gist create | rg github | pbcopy'
 
@@ -20,3 +23,9 @@ abbr -ag psmem 'ps aux | sort -nr -k 4 | head -10'
 abbr -ag pscpu 'ps -er -o pid,pcpu,comm | head -10'
 
 abbr -ag reload "source ~/.config/fish/config.fish | echo "Reloaded!" "
+
+abbr watts "/usr/sbin/ioreg -rw0 -c AppleSmartBattery | grep BatteryData | grep -o '"AdapterPower"=[0-9]*' | cut -c 16- | xargs -I %  lldb --batch -o "print/f %" | grep -o '$0 = [0-9.]*' | cut -c 6-"
+
+abbr speed "curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -"
+
+alias speedtest="curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -"
